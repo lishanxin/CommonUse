e6199cc8145cb6d763b92fb5439f33a8f8d199b2
lishanxin <34839411+lishanxin@users.noreply.github.com>
2019-05-31 11:04:05 +0800

    进程间通信之Socket

--- app/src/main/java/com/example/lishanxin/commonuse/process/MessengerActivity.java
+++ app/src/main/java/com/example/lishanxin/commonuse/process/MessengerActivity.java
@@ -20,6 +20,15 @@ import com.example.lishanxin.commonuse.IRemoteService;
 import com.example.lishanxin.commonuse.R;
 import com.example.lishanxin.commonuse.Student;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * https://developer.android.com/guide/components/bound-services.html#Binder
  */
@@ -81,4 +90,44 @@ public class MessengerActivity extends AppCompatActivity {
     }
 
 
+    public void socketTest(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //IO操作不能放在主线程执行
+                connectServer();
+            }
+        }).start();
+    }
+
+    private static void connectServer() {
+        //https://blog.csdn.net/hzw2017/article/details/81210979
+        try {
+            //1、创建客户端socket，指定服务端地址和端口
+            Socket socket = new Socket("localhost", 8888);
+            boolean connected = socket.isConnected(); //检查客户端与服务端是否连接成功
+            System.out.println(connected?"连接成功":"连接失败，请重试！");
+
+            //2、获取输出流，向服务器发送消息
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream),true);
+            writer.write("第一次来到广州\n");
+            writer.flush();
+
+            //3、获取输入流，并读取服务端的响应信息
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String info=reader.readLine();
+            System.out.println("客户端收到服务端回应："+info);
+
+
+            //4、关闭资源
+            outputStream.close();
+            writer.close();
+            inputStream.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/lishanxin/commonuse/process/MessengerService.java
+++ app/src/main/java/com/example/lishanxin/commonuse/process/MessengerService.java
@@ -9,6 +9,15 @@ import android.os.Messenger;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 /**
  * @author: Li Shanxin
@@ -36,6 +45,53 @@ public class MessengerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                serviceSocket();
+            }
+        }).start();
         return mMessenger.getBinder();
+    }
+
+
+    private void serviceSocket(){
+        try {
+            //1、创建ServerSocket对象，指定与客户端一样的端口号
+            ServerSocket serverSocket = new ServerSocket(8888);
+            //2、获取Socket实例
+            final Socket socket = serverSocket.accept();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //3、获取输入流，接受客户端发来的消息
+                        InputStream inputStream = socket.getInputStream();
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        BufferedReader reader = new BufferedReader(inputStreamReader);
+                        String info=reader.readLine();
+                        System.out.println("服务端收到客户端的信息： " +info);
+
+                        //4、获取输出流，向客户端发送消息回应
+                        OutputStream outputStream = socket.getOutputStream();
+                        PrintWriter writer = new PrintWriter(outputStream);
+                        writer.write("羊城欢迎你！"+"\n");
+                        writer.flush();
+
+                        //4、关闭IO资源
+                        inputStream.close();
+                        reader.close();
+                        outputStream.close();
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
